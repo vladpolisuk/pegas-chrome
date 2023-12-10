db2b2087ff31bd78f5fd507a400de78ebc8b5738
Vladislav Polishchuk <vlad@polisuk.ru>
2023-12-11 02:00:29 +0300

    fix

--- scripts/utils/getDateInformation.js
+++ scripts/utils/getDateInformation.js
@@ -1,9 +1,9 @@
 export function getDateInformation(number) {
-    const current = new Date(new Date().toUTCString());
-    const daysToShift = 7 * number;
-    const firstDay = new Date(current.setUTCDate(current.getUTCDate() - current.getUTCDay() + 1 + daysToShift));
-    const lastDay = new Date(current.setUTCDate(current.getUTCDate() - current.getUTCDay() + 7 + daysToShift));
+    const current = new Date()
+    const dayOffset = number * 7;
+    const firstDay = new Date(new Date().setUTCDate(current.getUTCDate() - current.getUTCDay() + 1 + dayOffset));
+    const lastDay = new Date(new Date().setUTCDate(current.getUTCDate() - current.getUTCDay() + 7 + dayOffset));
     const fromDate = `${firstDay.toISOString().substring(0, 10)}`;
-    const toDate = `${lastDay.toISOString().substring(0, 10)}`;
+    const toDate = `${lastDay.toISOString().substring(0, 10)}`;    
     return { fromDate, toDate };
 }
